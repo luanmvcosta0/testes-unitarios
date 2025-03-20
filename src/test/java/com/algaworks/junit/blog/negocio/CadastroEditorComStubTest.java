047b1cf66e2dd4e5e171986c0a76ba0956d5168c
luanmvcosta0 <luanmvcosta@gmail.com>
2025-03-20 16:16:24 -0300

    Simulando classes com Mockito

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComStubTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComStubTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class CadastroEditorTest {
+class CadastroEditorComStubTest {
 
     CadastroEditor cadastroEditor;
     ArmazenamentoEditorFixoEmMemoria armazenamentoEditor;
